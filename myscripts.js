e769f7ae58d10fd969387929aa5adbc9537fe8b4
lightspot-coder <214618438+lightspot-coder@users.noreply.github.com>
2025-11-20 10:17:36 +0100

    adding validation with js to the author name field

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,3 @@
-
 const Library_Table = document.getElementById("LibraryTable");
 let Library_Table_Body = document.getElementById("LibraryTable_Body");
 const body_container = document.getElementById("body_container");
@@ -8,8 +7,7 @@ let totalBooks = 0;
 // class book
 
 class Book {
-
-  constructor(author, title, numberOfPages, isReadIt){
+  constructor(author, title, numberOfPages, isReadIt) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
@@ -17,68 +15,74 @@ class Book {
     this.id = crypto.randomUUID();
   }
   changeReadStatus() {
-    if(this.isReadIt === "Yes")
-      this.isReadIt = "No";
-    else
-    this.isReadIt = "Yes";
+    if (this.isReadIt === "Yes") this.isReadIt = "No";
+    else this.isReadIt = "Yes";
   }
 }
 
-
-function addBookToLibrary(author, title, numberOfPages, isReadIt ) {
+function addBookToLibrary(author, title, numberOfPages, isReadIt) {
   // take params, create a book then store it in the array
   const newBook = new Book(author, title, numberOfPages, isReadIt);
   myLibrary.push(newBook);
   totalBooks += 1;
 }
 
-function removeBook(id){
-
-  
-  for(let i=0; i < totalBooks ; i++){
-    if(myLibrary[i].id === id){
+function removeBook(id) {
+  for (let i = 0; i < totalBooks; i++) {
+    if (myLibrary[i].id === id) {
       totalBooks -= 1;
-      console.log("The book " + myLibrary[i].title + " was erased. The library now have " + totalBooks + " books in total" );
-      myLibrary.splice(i,1);
+      console.log(
+        "The book " +
+          myLibrary[i].title +
+          " was erased. The library now have " +
+          totalBooks +
+          " books in total"
+      );
+      myLibrary.splice(i, 1);
       displayBooks();
       break;
     }
   }
-
-}function changeStatusButton(id){
-  for(let i=0; i < totalBooks ; i++){
-    if(myLibrary[i].id === id){
-        let oldStatus = myLibrary[i].isReadIt;
-        myLibrary[i].changeReadStatus();
-        let newSatus = myLibrary[i].isReadIt;
-        console.log("Change the status of the book " + myLibrary[i].title  + " : " + oldStatus + " to " + newSatus );
-        displayBooks();
-        break;
+}
+function changeStatusButton(id) {
+  for (let i = 0; i < totalBooks; i++) {
+    if (myLibrary[i].id === id) {
+      let oldStatus = myLibrary[i].isReadIt;
+      myLibrary[i].changeReadStatus();
+      let newSatus = myLibrary[i].isReadIt;
+      console.log(
+        "Change the status of the book " +
+          myLibrary[i].title +
+          " : " +
+          oldStatus +
+          " to " +
+          newSatus
+      );
+      displayBooks();
+      break;
     }
   }
-
 }
 
 function displayBooks() {
-    console.log("Total of books in the Library = " + totalBooks);
+  console.log("Total of books in the Library = " + totalBooks);
 
-    Library_Table_Body.remove();
-    Library_Table_Body = document.createElement("tbody");
-    Library_Table_Body.setAttribute("id","LibraryTable_Body");
-    
+  Library_Table_Body.remove();
+  Library_Table_Body = document.createElement("tbody");
+  Library_Table_Body.setAttribute("id", "LibraryTable_Body");
 
-    for(let i = 0; i < totalBooks ; i++){
-        /*console.log(myLibrary[i]);*/
-        const author_Cell = document.createElement("td");
-        author_Cell.textContent = myLibrary[i].author;
-        const title_Cell = document.createElement("td");
-        title_Cell.textContent = myLibrary[i].title;
-        const totalPages_Cell = document.createElement("td");
-        totalPages_Cell.textContent = myLibrary[i].numberOfPages;
-        //const isReadIt_Cell = document.createElement("td");
-        //isReadIt_Cell.textContent = myLibrary[i].isReadIt;
+  for (let i = 0; i < totalBooks; i++) {
+    /*console.log(myLibrary[i]);*/
+    const author_Cell = document.createElement("td");
+    author_Cell.textContent = myLibrary[i].author;
+    const title_Cell = document.createElement("td");
+    title_Cell.textContent = myLibrary[i].title;
+    const totalPages_Cell = document.createElement("td");
+    totalPages_Cell.textContent = myLibrary[i].numberOfPages;
+    //const isReadIt_Cell = document.createElement("td");
+    //isReadIt_Cell.textContent = myLibrary[i].isReadIt;
 
-        /*
+    /*
         const remove_book_Button = document.createElement("button");
         remove_book_Button.setAttribute("type","button");
         remove_book_Button.setAttribute("data",myLibrary[i].id);
@@ -90,20 +94,19 @@ function displayBooks() {
 
         });*/
 
-        const remove_book_Button = document.createElement("img");
-        remove_book_Button.setAttribute("src","img/alpha-x-box.svg");
-        remove_book_Button.setAttribute("width","40px");
-        remove_book_Button.setAttribute("height","40px");
-        remove_book_Button.setAttribute("data",myLibrary[i].id);
-        remove_book_Button.addEventListener("click",() => {
+    const remove_book_Button = document.createElement("img");
+    remove_book_Button.setAttribute("src", "img/alpha-x-box.svg");
+    remove_book_Button.setAttribute("width", "40px");
+    remove_book_Button.setAttribute("height", "40px");
+    remove_book_Button.setAttribute("data", myLibrary[i].id);
+    remove_book_Button.addEventListener("click", () => {
+      console.log(
+        "Removing the book with id " + remove_book_Button.getAttribute("data")
+      );
+      removeBook(remove_book_Button.getAttribute("data"));
+    });
 
-            console.log("Removing the book with id " + remove_book_Button.getAttribute("data"));
-            removeBook(remove_book_Button.getAttribute("data"));
-
-        });
-
-
-        /*
+    /*
         const change_status_Button = document.createElement("button");
         change_status_Button.setAttribute("type","button");
         change_status_Button.setAttribute("data",myLibrary[i].id);
@@ -114,52 +117,49 @@ function displayBooks() {
             changeStatusButton(change_status_Button.getAttribute("data"));
 
         });*/
-        const change_status_Button = document.createElement("img");
-        change_status_Button.setAttribute("src","img/arrow-down-drop-circle.svg");
-        change_status_Button.setAttribute("data",myLibrary[i].id);
-        change_status_Button.setAttribute("width","20px");
-        change_status_Button.setAttribute("height","20px");
-        change_status_Button.textContent = "Change read status";
-        change_status_Button.addEventListener("click",() => {
+    const change_status_Button = document.createElement("img");
+    change_status_Button.setAttribute("src", "img/arrow-down-drop-circle.svg");
+    change_status_Button.setAttribute("data", myLibrary[i].id);
+    change_status_Button.setAttribute("width", "20px");
+    change_status_Button.setAttribute("height", "20px");
+    change_status_Button.textContent = "Change read status";
+    change_status_Button.addEventListener("click", () => {
+      console.log(
+        "Changing the status of the book with id " +
+          change_status_Button.getAttribute("data")
+      );
+      changeStatusButton(change_status_Button.getAttribute("data"));
+    });
 
-            console.log("Changing the status of the book with id " + change_status_Button.getAttribute("data"));
-            changeStatusButton(change_status_Button.getAttribute("data"));
+    const newBook_Row = document.createElement("tr");
+    newBook_Row.appendChild(author_Cell);
+    newBook_Row.appendChild(title_Cell);
+    newBook_Row.appendChild(totalPages_Cell);
+    //newBook_Row.appendChild(isReadIt_Cell);
 
-        });
+    const read_status_cell = document.createElement("td");
+    const read_status_p = document.createElement("p");
+    read_status_p.textContent = myLibrary[i].isReadIt;
 
+    const read_status_div = document.createElement("div");
+    read_status_div.setAttribute("id", "read-status-div");
 
+    read_status_div.appendChild(read_status_p);
+    read_status_div.appendChild(change_status_Button);
 
-        const newBook_Row = document.createElement("tr");
-        newBook_Row.appendChild(author_Cell);
-        newBook_Row.appendChild(title_Cell);
-        newBook_Row.appendChild(totalPages_Cell);
-        //newBook_Row.appendChild(isReadIt_Cell);
+    read_status_cell.appendChild(read_status_div);
 
-        const read_status_cell = document.createElement("td");
-        const read_status_p = document.createElement("p");
-        read_status_p.textContent = myLibrary[i].isReadIt;
+    newBook_Row.appendChild(read_status_cell);
 
-        const read_status_div = document.createElement("div");
-        read_status_div.setAttribute("id","read-status-div");
-        
-        
-        read_status_div.appendChild(read_status_p);
-        read_status_div.appendChild(change_status_Button);
+    const book_buttons_div = document.createElement("div");
+    book_buttons_div.setAttribute("id", "book-Buttons-div");
+    book_buttons_div.appendChild(remove_book_Button);
+    //book_buttons_div.appendChild(change_status_Button);
+    newBook_Row.appendChild(book_buttons_div);
 
-        read_status_cell.appendChild(read_status_div);
-        
-        newBook_Row.appendChild(read_status_cell);
-
-        const book_buttons_div = document.createElement("div");
-        book_buttons_div.setAttribute("id","book-Buttons-div");
-        book_buttons_div.appendChild(remove_book_Button);
-        //book_buttons_div.appendChild(change_status_Button);
-        newBook_Row.appendChild(book_buttons_div);
-        
-        
-        Library_Table_Body.appendChild(newBook_Row);
-    }
-    Library_Table.appendChild(Library_Table_Body);
+    Library_Table_Body.appendChild(newBook_Row);
+  }
+  Library_Table.appendChild(Library_Table_Body);
 }
 
 const author1 = "shakespeare";
@@ -167,17 +167,16 @@ const title1 = "Romeo and Juliet ";
 const numberOfPages1 = 400;
 const isReadIt1 = "Yes";
 
-addBookToLibrary(author1, title1, numberOfPages1, isReadIt1 ) 
+addBookToLibrary(author1, title1, numberOfPages1, isReadIt1);
 
 const author2 = "Asimov";
 const title2 = "Fundation";
 const numberOfPages2 = 600;
 const isReadIt2 = "No";
 
-addBookToLibrary(author2, title2, numberOfPages2, isReadIt2 ) 
+addBookToLibrary(author2, title2, numberOfPages2, isReadIt2);
 
 displayBooks();
-
 
 /* prevent the normal action from submit button (prevent sending the info to the server)*/
 
@@ -185,19 +184,39 @@ function buttonClick(event) {
   event.preventDefault();
 }
 
-function submitForm(){
+function submitForm() {
+  const author = document.getElementById("author_name").value;
+  const title = document.getElementById("title_book").value;
+  const numberOfPages = document.getElementById("total_pages").value;
+  const isReadIt = document.getElementById("select_option").value;
 
-    const author = document.getElementById("author_name").value;
-    const title = document.getElementById("title_book").value;
-    const numberOfPages = document.getElementById("total_pages").value;
-    const isReadIt = document.getElementById("select_option").value;
-    
-    addBookToLibrary(author, title, numberOfPages, isReadIt );
-    displayBooks();
+  addBookToLibrary(author, title, numberOfPages, isReadIt);
+  displayBooks();
 }
+
+let addBook = false;
 
 const addBookButton = document.getElementById("addBook_button");
 
-addBookButton.addEventListener("click", buttonClick, false);
-addBookButton.addEventListener("click", submitForm, false);
+addBookButton.addEventListener("click", (event) => {
+  console.log(addBook);
+  if (addBook) {
+    event.preventDefault();
+    submitForm();
+  }
+});
+//addBookButton.addEventListener("click", submitForm);
 
+const input_AuthorName = document.getElementById("author_name");
+input_AuthorName.required = true;
+
+input_AuthorName.addEventListener("input", (event) => {
+  if (input_AuthorName.validity.valueMissing) {
+    addBook = false;
+    console.log(input_AuthorName);
+    input_AuthorName.setCustomValidity("The author name must be filled!");
+  } else {
+    addBook = true;
+    input_AuthorName.setCustomValidity("");
+  }
+});
